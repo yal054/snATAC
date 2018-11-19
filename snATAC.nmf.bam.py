5820219b60cdb06c84ea775a6acf1bd7356a94de
Yang Li <yangli1@tscc-login2.sdsc.edu>
2018-11-19 10:20:57 -0800

    change bam

--- snATAC.nmf.bam.py
+++ snATAC.nmf.bam.py
@@ -48,9 +48,8 @@ def generate_bam_worker(bamf, o_stat_H, r, prefix):
 			obam.write(b)
 	obam.close()
 	bamF.close()
-  print("metaCell =", n, " witting finished.")
+	print("metaCell =", n, " witting finished.")
 
- 
 
 if __name__ == "__main__":
 	"""filter bam based on QNAMES"""
